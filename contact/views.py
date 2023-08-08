e586de6de38e5354e4c2348d93e1151b1f0c6df9
Cem <meck4181@gmail.com>
2023-08-08 08:48:04 +0300

    refactor: update contact form

--- contact/views.py
+++ contact/views.py
@@ -2,7 +2,9 @@ from django.http import JsonResponse
 
 
 def contact_form(request):
-    context = {'success': True,
-               'message': 'Contact form sen successfully.',}
+    context = {
+        'success': False,
+        'message': 'Contact form sent successfully.'
+    }
     return JsonResponse(context)
 # Create your views here.
