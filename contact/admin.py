aaa5a575baef2627487e413727afb15b2f3a5061
Cem <meck4181@gmail.com>
2023-08-09 22:10:26 +0300

    feat: create message model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from contact.models import Message
+
+
+# Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'subject', 'message', 'updated_date', 'created_date']
+    search_fields = ['name', 'email', 'subject', 'message', ]
+
+    class Meta:
+        model = Message
+
+
+from django.contrib import admin
 
 # Register your models here.
