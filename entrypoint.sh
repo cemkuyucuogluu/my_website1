6d7e25b97885954a956c2b4055f26415b06abf2d
Cem <meck4181@gmail.com>
2023-08-13 15:32:53 +0300

    fix: entrypoint.sh permission

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 python manage.py migrate --noinput
 
-python manage.py createsuperuser --username="$DJANGO_SUPER_USERNAME" --email="$DJANGO_SUPER_USER_EMAIL" --no-input
+python manage.py createsuperuser --username="admin" --email="cemkuyucuogluu@gmail.com" --password='623262@@' --no-input
 
 exec "$@"
+
+
